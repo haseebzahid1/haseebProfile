157dbf2052570c45228ab0ddc83a3146e6d45782
haseebzahid1 <mmianhaseeb444@gmail.com>
2022-04-12 23:05:38 +0500

    navbar added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-      // Init Scrollspy
+$(window).on('scroll', function () {
+  if ($(this).scrollTop() >= 250) { // Set position from top to add class
+      $('header').addClass('header-appear');
+  }
+  else {
+      $('header').removeClass('header-appear');
+  }
+});
+
+function toggle(){
+  var trailer = document.querySelector(".trailer")
+  trailer.classList.toggle("active")
+}
+
+
+
+// Init Scrollspy
       $('body').scrollspy({
         target: '#main-nav'
     });
